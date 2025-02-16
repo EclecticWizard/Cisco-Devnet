4a39a241a390ae4b40c5987e7f844e304cee2052
EclecticWizard <hodgkinsj95@proton.me>
2025-02-16 17:43:45 +0000

    myLocation.py

---
+++ Mod3/myLocation.py
@@ -0,0 +1,9 @@
+class Location:
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+
+
+loc = Location("yourName", "yourCountry")
+print(loc.name)
+print(loc.country)
