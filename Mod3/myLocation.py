f24e965d6f0e73294423e9a3bd60e5adac470d1f
EclecticWizard <hodgkinsj95@proton.me>
2025-02-16 18:56:54 +0000

    finished lab 3.4.6

--- Mod3/myLocation.py
+++ Mod3/myLocation.py
@@ -3,7 +3,19 @@ class Location:
         self.name = name
         self.country = country
 
+    def myLocation(self):
+        print("Hi my name is " + self.name + " and I live in " + self.country + ".")
+
 
 loc = Location("yourName", "yourCountry")
 print(loc.name)
 print(loc.country)
+
+loc1 = Location("Steve", "Portugal")
+loc2 = Location("Dave", "Paris")
+loc3 = Location("Gwen", "Wales")
+
+loc1.myLocation()
+loc2.myLocation()
+loc3.myLocation()
+loc.myLocation()
